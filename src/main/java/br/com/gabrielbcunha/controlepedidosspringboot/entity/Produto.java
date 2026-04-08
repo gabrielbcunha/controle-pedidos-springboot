f9f596c32de8ad4409282ede901d9545d2b0a886
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-08 16:59:39 -0300

    feat(entity): cria entidade Produto

---
+++ src/main/java/br/com/gabrielbcunha/controlepedidosspringboot/entity/Produto.java
@@ -0,0 +1,29 @@
+package br.com.gabrielbcunha.controlepedidosspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="produtos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="nome produto",nullable = false, length = 150)
+    private String nome;
+
+    @Column(name="preco_produto", nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+}
