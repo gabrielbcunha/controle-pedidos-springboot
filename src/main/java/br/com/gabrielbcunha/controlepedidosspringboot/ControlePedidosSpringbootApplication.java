e213de67cfa6358247b432ac1d952cb9f4e7febe
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-08 15:51:45 -0300

    first commit

---
+++ src/main/java/br/com/gabrielbcunha/controlepedidosspringboot/ControlePedidosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielbcunha.controlepedidosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePedidosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePedidosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrielbcunha/controlepedidosspringboot/ControlePedidosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielbcunha.controlepedidosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePedidosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
