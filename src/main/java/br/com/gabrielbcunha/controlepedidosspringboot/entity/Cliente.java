17b2f41ae60bda1db0f6ef56b5fcb096ecfe1988
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-08 16:14:56 -0300

    feat(entity): cria entidade Cliente

---
+++ src/main/java/br/com/gabrielbcunha/controlepedidosspringboot/entity/Cliente.java
@@ -0,0 +1,27 @@
+package br.com.gabrielbcunha.controlepedidosspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="nome_cliente", nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 20)
+    private String numeroContato;
+
+}
