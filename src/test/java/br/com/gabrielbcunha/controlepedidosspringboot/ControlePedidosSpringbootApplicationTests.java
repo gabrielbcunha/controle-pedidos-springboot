983caf4ea7ed470046c7a67c223d7cd2e1991a28
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-08 15:55:44 -0300

    Primeiras configurações

--- src/test/java/br/com/gabrielbcunha/controlepedidosspringboot/ControlePedidosSpringbootApplicationTests.java
+++ src/test/java/br/com/gabrielbcunha/controlepedidosspringboot/ControlePedidosSpringbootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ControlePedidosSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
