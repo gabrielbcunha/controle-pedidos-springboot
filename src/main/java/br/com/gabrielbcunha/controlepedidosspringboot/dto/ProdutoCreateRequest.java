b9ed1ac991ac4fa6100519c86fbb78d072ec1473
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-13 10:54:18 -0300

    fix(dto): altera NotBlank para NotNull

--- src/main/java/br/com/gabrielbcunha/controlepedidosspringboot/dto/ProdutoCreateRequest.java
+++ src/main/java/br/com/gabrielbcunha/controlepedidosspringboot/dto/ProdutoCreateRequest.java
@@ -1,6 +1,7 @@
 package br.com.gabrielbcunha.controlepedidosspringboot.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
